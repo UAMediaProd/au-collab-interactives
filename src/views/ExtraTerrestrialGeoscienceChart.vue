75d8f30819df0e5e31da3a715641ad7433d185f6
Tim Churchward <git@timchurchward.com.au>
2025-02-21 14:09:50 +1100

    Added line labels and glow effect on hover/click

--- src/views/ExtraTerrestrialGeoscienceChart.vue
+++ src/views/ExtraTerrestrialGeoscienceChart.vue
@@ -7,23 +7,49 @@ const visibility = ref({
     basalt: true,
     organics: true,
     water: true,
-    labels: true
+    labels: true,
+    hotspots: true
+});
+
+const glow = ref({
+    washload: false,
+    suspended: false,
+    bedload: false,
 });
 
 const toggleVisibility = (elementId) => {
     visibility.value[elementId] = !visibility.value[elementId];
+}
+
+const toggleGlow = (elementId) => {
+    glow.value[elementId] = !glow.value[elementId];
 }
 </script>
 
 <template>
     <!-- <button id="button" class="border" @click="toggleVisibility($event)">Toggle visibility</button> -->
     <!-- max-w-[900px] mx-auto flex h-screen items-center justify-center -->
-    <div id="graph" class="max-w-[900px] mx-auto flex flex-col border">
+    <h2 class="text-4xl sm:text-5xl text-center text-primary p-3 sm:p-5">Particle movement in fluids</h2>
+    <div id="graph" class="max-w-[900px] mx-auto flex flex-col select-none">
         <div class="flex justify-between items-center">
             <span class="rotated-text px-3 sm:p-5 sm:text-xl text-primary">
                 Frictional shear velocity (cm/s)
             </span>
-            <div class="flex-auto">
+            <div class="flex-auto relative">
+                <div class="absolute inset-0 h-full w-full z-10 flex" v-show="visibility.labels">
+                    <p class="text-sm sm:text-lg text-primary absolute top-[27%] left-[76%] -translate-x-1/2 -translate-y-1/2 bg-primary-lighter hover:bg-[#D0CFDCB3] px-2 py-1 sm:px-3 sm:py-1 rounded backdrop-blur-sm bg-opacity-70 text-nowrap"
+                        @mouseover="glow.washload = true" @mouseleave="glow.washload = false"
+                        @click="toggleGlow('washload')">Washload</p>
+                    <p class="text-sm sm:text-lg text-primary absolute top-[48%] left-[76%] -translate-x-1/2 -translate-y-1/2 bg-primary-lighter hover:bg-[#D0CFDCB3] px-2 py-1 sm:px-3 sm:py-1 rounded backdrop-blur-sm bg-opacity-70 text-nowrap"
+                    @mouseover="glow.suspended = true" @mouseleave="glow.suspended = false"    
+                    @click="toggleGlow('suspended')">Suspended load</p>
+                    <p class="text-sm sm:text-lg text-primary absolute top-[64%] left-[76%] -translate-x-1/2 -translate-y-1/2 bg-primary-lighter hover:bg-[#D0CFDCB3] px-2 py-1 sm:px-3 sm:py-1 rounded backdrop-blur-sm bg-opacity-70 text-nowrap"
+                    @mouseover="glow.bedload = true" @mouseleave="glow.bedload = false"    
+                    @click="toggleGlow('bedload')">Bedload</p>
+                    <p
+                        class="text-sm sm:text-lg text-primary absolute top-[82%] left-[76%] -translate-x-1/2 -translate-y-1/2 bg-primary-lighter hover:bg-[#D0CFDCB3] px-2 py-1 sm:px-3 sm:py-1 rounded backdrop-blur-sm bg-opacity-70 text-nowrap">
+                        No motion</p>
+                </div>
                 <!-- <img src="@/assets/chart.svg" alt="Extra Terrestrial Geoscience Chart" /> -->
                 <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 405 343.7">
                     <g>
@@ -116,41 +142,42 @@ const toggleVisibility = (elementId) => {
                                     </text>
                                 </g>
                                 <transition name="fade">
-                                    <g v-show="visibility.quartz">
-                                        <path class="cls-2"
+                                    <g v-show="visibility.quartz" class="quartz">
+                                        <path class="cls-2 bedload" :class="glow.bedload ? 'glow' : ''"
                                             d="M32,308.1c50.2-25,103.3-48.2,158.9-55.9,11.5-1.6,23.4-1.5,34.3-5.4,29.8-10.7,47.8-27.7,77.2-39.6,15.4-6.2,62.6-29.1,78.6-35.1" />
-                                        <path class="cls-2"
+                                        <path class="cls-2 suspended" :class="glow.suspended ? 'glow' : ''"
                                             d="M187.4,252.7c21.7-27.8,44-55.3,73.9-74,8.1-5.1,16.7-9.3,25.3-13.5,32-15.7,61.8-29.6,94.4-44.1" />
-                                        <path class="cls-2"
+                                        <path class="cls-2 washload" :class="glow.washload ? 'glow' : ''"
                                             d="M129.3,266.6c16.3-30.5,35.6-59.3,55.7-87.5,14.1-19.9,28.8-39.8,46.6-56.4,24.5-22.8,49.2-34.1,79.4-48.6,20.6-9.9,48.4-24.5,70-32" />
                                     </g>
                                 </transition>
                                 <transition name="fade">
-                                    <g v-show="visibility.basalt">
-                                        <path class="cls-3"
+                                    <g v-show="visibility.basalt" class="basalt">
+                                        <path class="cls-3 bedload" :class="glow.bedload ? 'glow' : ''"
                                             d="M32,319.1c60.1-28.9,120.3-53.5,184.8-62.2,9.3-1.3,22.8-4.5,31.2-8.8,12-6,21.6-11.1,31-17,23.2-14.5,42.8-20.9,67.9-31.7,11.9-5.2,22.2-10.1,34.1-15.3" />
-                                        <path class="cls-3"
+                                        <path class="cls-3 suspended" :class="glow.suspended ? 'glow' : ''"
                                             d="M195,260.1c22.7-35.1,52.6-63.5,90-83,23-12,87.7-41.2,96-45" />
-                                        <path class="cls-3"
+                                        <path class="cls-3 washload" :class="glow.washload ? 'glow' : ''"
                                             d="M381,54.1c-100,41-131.2,67.3-137,72-10.7,8.7-19.4,19.5-28.3,30-28.1,32.9-59.8,80.1-78.7,119" />
                                     </g>
                                 </transition>
                                 <transition name="fade">
-                                    <g v-show="visibility.organics">
-                                        <path class="cls-4" d="M189,274.1c20.3-34,51.8-61.1,86-81s70.4-32.6,106-50" />
-                                        <path class="cls-4"
-                                            d="M130,289c7.1-16,17.2-32,26.7-46.7,23.7-36.6,46.7-74.4,80.3-102.2,29.7-24.6,66.5-40,101.6-55.8,14.3-6.4,28.1-12.7,42.4-19.2" />
-                                        <path class="cls-4"
+                                    <g v-show="visibility.organics" class="organics">
+                                        <path class="cls-4 bedload" :class="glow.bedload ? 'glow' : ''"
                                             d="M50.4,321c33-15,66.9-28.2,101.4-39.4,13.9-4.5,24.6-5.7,39.1-7.5,16.2-2,35-5.7,50-12,5.7-2.4,17.6-9.9,23-13,42-24,73-33,117-54" />
+                                        <path class="cls-4 suspended" :class="glow.suspended ? 'glow' : ''"
+                                            d="M189,274.1c20.3-34,51.8-61.1,86-81s70.4-32.6,106-50" />
+                                        <path class="cls-4 washload" :class="glow.washload ? 'glow' : ''"
+                                            d="M130,289c7.1-16,17.2-32,26.7-46.7,23.7-36.6,46.7-74.4,80.3-102.2,29.7-24.6,66.5-40,101.6-55.8,14.3-6.4,28.1-12.7,42.4-19.2" />
                                     </g>
                                 </transition>
                                 <transition name="fade">
-                                    <g v-show="visibility.water">
-                                        <path class="cls-5"
+                                    <g v-show="visibility.water" class="water">
+                                        <path class="cls-5 bedload" :class="glow.bedload ? 'glow' : ''"
                                             d="M71.3,322.5c41.3-18.9,76.3-35.4,123.7-42.4,18.5-2.7,41.9-5.5,59-13,14.4-6.3,26-17,39.5-25,8.4-5,17.5-8.8,26.5-12.6,20.6-8.8,41.3-17.6,62-26.4" />
-                                        <path class="cls-5"
+                                        <path class="cls-5 suspended" :class="glow.suspended ? 'glow' : ''"
                                             d="M195.3,280.1c20.2-33,47.7-60,83.7-80,33.8-18.8,66.1-33.7,102-48" />
-                                        <path class="cls-5"
+                                        <path class="cls-5 washload" :class="glow.washload ? 'glow' : ''"
                                             d="M139.1,293.9c26-50,47.4-83.2,77.9-120.8,5.7-7,11.7-13.6,18-20,27-27.6,110.5-63.9,146-79" />
                                     </g>
                                 </transition>
@@ -184,34 +211,45 @@ const toggleVisibility = (elementId) => {
             <div class="bg-primary-lighter p-5 pb-2 mb-5 rounded max-w-xl">
                 <p class="text-center text-lg pb-2"><strong>Visibility toggles</strong></p>
                 <div class="flex flex-col sm:flex-row sm:flex-wrap justify-center">
-                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10" @click="toggleVisibility('quartz')">
+                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10"
+                        @click="toggleVisibility('quartz')">
                         <span class="w-5 inline-block text-center"><i class="fal"
                                 :class="visibility.quartz ? 'fa-eye' : 'fa-eye-slash'"></i></span>
                         <span class="w-4 h-4 ml-2 bg-[#93278f] rounded-full"></span>
                         <span class="ml-2">Quartz in H<sub>2</sub>O on Earth</span>
                     </button>
-                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10" @click="toggleVisibility('basalt')">
+                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10"
+                        @click="toggleVisibility('basalt')">
                         <span class="w-5 inline-block text-center"><i class="fal"
                                 :class="visibility.basalt ? 'fa-eye' : 'fa-eye-slash'"></i></span>
                         <span class="w-4 h-4 ml-2 bg-[#ed1c24] rounded-full"></span>
                         <span class="ml-2">Basalt in H<sub>2</sub>O on Mars</span>
                     </button>
-                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10" @click="toggleVisibility('organics')">
+                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10"
+                        @click="toggleVisibility('organics')">
                         <span class="w-5 inline-block text-center"><i class="fal"
                                 :class="visibility.organics ? 'fa-eye' : 'fa-eye-slash'"></i></span>
                         <span class="w-4 h-4 ml-2 bg-[#09892a] rounded-full"></span>
                         <span class="ml-2">Organics in CH<sub>4</sub>N<sub>2</sub> on Titan</span>
                     </button>
-                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10" @click="toggleVisibility('water')">
+                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10"
+                        @click="toggleVisibility('water')">
                         <span class="w-5 inline-block text-center"><i class="fal"
                                 :class="visibility.water ? 'fa-eye' : 'fa-eye-slash'"></i></span>
                         <span class="w-4 h-4 ml-2 bg-[#1448ff] rounded-full"></span>
                         <span class="ml-2">H<sub>2</sub>O in CH<sub>4</sub>N<sub>2</sub> on Titan</span>
                     </button>
-                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10" @click="toggleVisibility('labels')">
+                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10"
+                        @click="toggleVisibility('labels')">
                         <span class="w-5 inline-block text-center"><i class="fal"
                                 :class="visibility.labels ? 'fa-eye' : 'fa-eye-slash'"></i></span>
-                        <span class="ml-2">Labels (Coming soon)</span>
+                        <span class="ml-2">Labels</span>
+                    </button>
+                    <button class="p-3 rounded inline-flex items-center hover:bg-primary-light/10"
+                        @click="toggleVisibility('hotspots')">
+                        <span class="w-5 inline-block text-center"><i class="fal"
+                                :class="visibility.hotspots ? 'fa-eye' : 'fa-eye-slash'"></i></span>
+                        <span class="ml-2">Hotspots (Coming soon)</span>
                     </button>
                 </div>
             </div>
@@ -221,6 +259,27 @@ const toggleVisibility = (elementId) => {
 
 
 <style>
+.quartz .glow {
+    filter: drop-shadow(0px 0px 3px #93278fcc)
+}
+
+.basalt .glow {
+    filter: drop-shadow(0px 0px 3px #ed1c24cc)
+}
+
+.organics .glow {
+    filter: drop-shadow(0px 0px 3px #09892acc)
+}
+
+.water .glow {
+    filter: drop-shadow(0px 0px 3px #1448ffcc)
+}
+
+path {
+    transition: filter 0.2s ease;
+}
+
+
 /* Add transition classes for fade effect */
 .fade-enter-active,
 .fade-leave-active {
